0ac314e49b796aeaa450620548d1beb3d1972e12
melvinjim <melvinjimenezang@hotmail.com>
2022-05-23 14:44:59 -0500

    corrections in the code

--- random_colors.go
+++ random_colors.go
@@ -8,15 +8,21 @@ import (
 var colors = [16]string{"rojo","verde","azul","salmon","gris","negro","veige","morado","blanco","turquesa","amarillo","neon","melocoton","diamante","ruby","escarlata"}
 
 func main() {
+	numberRandom()
+}
+
+func numberRandom(){
 	rand.Seed(time.Now().UnixNano())
 	var random = randomInt(0, 15)
-	if (random & 0x01 == 1){
-		main()
+
+	if (random % 2 == 1){
+		numberRandom()
 	} else {
 		fmt.Println(colors[random])
 	}
 }
 
 func randomInt(min, max int) int {
-	return min + rand.Intn(max-min)
-}
+	var number = min + rand.Intn(max-min)
+	return number
+} 
