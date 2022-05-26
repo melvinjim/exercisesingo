ea0a056a0d33120dc5a2e228ef4f817d8793f6cf
melvinjim <melvinjimenezang@hotmail.com>
2022-05-26 08:25:04 -0500

    correcctions

--- exercise/main.go
+++ exercise/main.go
@@ -21,34 +21,4 @@ func main() {
 		var suma = firstNumber + secondNumber
 		fmt.Printf("el resultado es: %d\n", suma)
 	}
-
-	if respuesta == "resta" {
-		fmt.Print("¿Cuál es el primer numero desea restar? ")
-		fmt.Scanln(&firstNumber)
-		fmt.Print("¿Cuál es el segundo numero que desea restar? ")
-		fmt.Scanln(&secondNumber)
-
-		var resta = firstNumber - secondNumber
-		fmt.Printf("el resultado es: %d\n", resta)
-	}
-
-	if respuesta == "multiplicacion" {
-		fmt.Print("¿Cuál es el primer desea multiplicar? ")
-		fmt.Scanln(&firstNumber)
-		fmt.Print("¿Cuál es el segundo numero que desea multiplicar? ")
-		fmt.Scanln(&secondNumber)
-
-		var multiplications = firstNumber * secondNumber
-		fmt.Printf("el resultado es: %d\n", multiplications)
-	}
-
-	if respuesta == "division" {
-		fmt.Print("¿Cuál es el primer desea dividir? ")
-		fmt.Scanln(&firstNumber)
-		fmt.Print("¿Cuál es el segundo numero que desea dividir? ")
-		fmt.Scanln(&secondNumber)
-
-		var dividir = firstNumber / secondNumber
-		fmt.Printf("el resultado es: %d\n", dividir)
-	}
 }
