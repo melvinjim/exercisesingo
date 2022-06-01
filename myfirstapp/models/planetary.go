8da8e12b2071e7da5d37fa499baf7d5d21f8815e
melvinjim <melvinjimenezang@hotmail.com>
2022-06-01 17:58:09 -0500

    tests were performed on the my first app folder

--- myfirstapp/models/planetary.go
+++ myfirstapp/models/planetary.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
 type Planetary struct {
 	Date           string `json:"date"`
 	Explanation    string `json:"explanation"`
@@ -8,4 +14,21 @@ type Planetary struct {
 	ServiceVersion string `json:"service_version"`
 	Title          string `json:"title"`
 	URL            string `json:"url"`
+}
+
+func GetPlanetary() Planetary {
+	url := "https://api.nasa.gov/planetary/apod?api_key=e2iQ0x7l5wDJz7bKZBkB5DL8DI5f6dSK9fFVGdNZ"
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", "bearer")
+
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+
+	data, _ := ioutil.ReadAll(resp.Body)
+
+	var planetary Planetary
+	json.Unmarshal(data, &planetary)
+
+	return planetary
 }
--- myfirstapp/models/planetary_test.go
+++ myfirstapp/models/planetary_test.go
@@ -1,25 +1,12 @@
 package models_test
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"myfirstapp/models"
-	"net/http"
 	"testing"
 )
 
 func TestDate(t *testing.T) {
-	url := "https://api.nasa.gov/planetary/apod?api_key=e2iQ0x7l5wDJz7bKZBkB5DL8DI5f6dSK9fFVGdNZ"
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("Authorization", "bearer")
-
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-
-	data, _ := ioutil.ReadAll(resp.Body)
-
-	var planetary models.Planetary
-	json.Unmarshal(data, &planetary)
+	planetary := models.GetPlanetary()
 
 	result := planetary.Date
 	expectedResult := "2022-06-01"
@@ -29,17 +16,7 @@ func TestDate(t *testing.T) {
 }
 
 func TestExplanation(t *testing.T) {
-	url := "https://api.nasa.gov/planetary/apod?api_key=e2iQ0x7l5wDJz7bKZBkB5DL8DI5f6dSK9fFVGdNZ"
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("Authorization", "bearer")
-
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-
-	data, _ := ioutil.ReadAll(resp.Body)
-
-	var planetary models.Planetary
-	json.Unmarshal(data, &planetary)
+	planetary := models.GetPlanetary()
 
 	result := planetary.Explanation
 	expectedResult := "It wasn't the storm of the century -- but it was a night to remember. Last night was the peak of the Tau Herculids meteor shower, a usually modest dribble of occasional meteors originating from the disintegrating Comet 73P/Schwassmann-Wachmann 3. This year, calculations showed that the Earth might be passing through a particularly dense stream of comet debris -- at best creating a storm of bright meteors streaking out from the constellation of Hercules. What actually happened fell short of a meteor storm, but could be called a decent meteor shower. Featured here is a composite image taken at Kitt Peak National Observatory in Arizona, USA accumulated over 2.5 hours very late on May 30.  Over that time, 19 Tau Herculids meteors were captured, along with 4 unrelated meteors. (Can you find them?)  In the near foreground is the Bok 2.3-meter Telescope with the 4.0-meter Mayall Telescope just behind it. Next year, the annual Tau Herculids are expected to return to its normal low rate, with the next active night forecast for 2049."
@@ -48,20 +25,8 @@ func TestExplanation(t *testing.T) {
 	}
 }
 
-// fmt.Println(string(data))
-
 func TestHdurl(t *testing.T) {
-	url := "https://api.nasa.gov/planetary/apod?api_key=e2iQ0x7l5wDJz7bKZBkB5DL8DI5f6dSK9fFVGdNZ"
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("Authorization", "bearer")
-
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-
-	data, _ := ioutil.ReadAll(resp.Body)
-
-	var planetary models.Planetary
-	json.Unmarshal(data, &planetary)
+	planetary := models.GetPlanetary()
 
 	result := planetary.Hdurl
 	expectedResult := "https://apod.nasa.gov/apod/image/2206/TauHerMeteors_Lyu_1280.jpg"
@@ -71,17 +36,7 @@ func TestHdurl(t *testing.T) {
 }
 
 func TestMediaType(t *testing.T) {
-	url := "https://api.nasa.gov/planetary/apod?api_key=e2iQ0x7l5wDJz7bKZBkB5DL8DI5f6dSK9fFVGdNZ"
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("Authorization", "bearer")
-
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-
-	data, _ := ioutil.ReadAll(resp.Body)
-
-	var planetary models.Planetary
-	json.Unmarshal(data, &planetary)
+	planetary := models.GetPlanetary()
 
 	result := planetary.MediaType
 	expectedResult := "image"
@@ -91,17 +46,7 @@ func TestMediaType(t *testing.T) {
 }
 
 func TestServiceVersion(t *testing.T) {
-	url := "https://api.nasa.gov/planetary/apod?api_key=e2iQ0x7l5wDJz7bKZBkB5DL8DI5f6dSK9fFVGdNZ"
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("Authorization", "bearer")
-
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-
-	data, _ := ioutil.ReadAll(resp.Body)
-
-	var planetary models.Planetary
-	json.Unmarshal(data, &planetary)
+	planetary := models.GetPlanetary()
 
 	result := planetary.ServiceVersion
 	expectedResult := "v1"
@@ -111,17 +56,7 @@ func TestServiceVersion(t *testing.T) {
 }
 
 func TestTitle(t *testing.T) {
-	url := "https://api.nasa.gov/planetary/apod?api_key=e2iQ0x7l5wDJz7bKZBkB5DL8DI5f6dSK9fFVGdNZ"
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("Authorization", "bearer")
-
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-
-	data, _ := ioutil.ReadAll(resp.Body)
-
-	var planetary models.Planetary
-	json.Unmarshal(data, &planetary)
+	planetary := models.GetPlanetary()
 
 	result := planetary.Title
 	expectedResult := "Tau Herculids Meteors over Kitt Peak Telescopes"
@@ -131,17 +66,7 @@ func TestTitle(t *testing.T) {
 }
 
 func TestURl(t *testing.T) {
-	url := "https://api.nasa.gov/planetary/apod?api_key=e2iQ0x7l5wDJz7bKZBkB5DL8DI5f6dSK9fFVGdNZ"
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("Authorization", "bearer")
-
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-
-	data, _ := ioutil.ReadAll(resp.Body)
-
-	var planetary models.Planetary
-	json.Unmarshal(data, &planetary)
+	planetary := models.GetPlanetary()
 
 	result := planetary.URL
 	expectedResult := "https://apod.nasa.gov/apod/image/2206/TauHerMeteors_Lyu_1080.jpg"
